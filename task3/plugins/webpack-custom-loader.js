f26f94da47f71ecfbab089acc7726aaf9e962221
Yauheni Troshka <Yauheni_Troshka@epam.com>
2017-12-11 19:00:59 +0300

    Add url loader

--- task3/plugins/webpack-custom-loader.js
+++ task3/plugins/webpack-custom-loader.js
@@ -1,7 +1,4 @@
 module.exports = function (source) {
-    console.log(source);
-    console.log(typeof source === "string");
-    console.log(JSON.parse(source));
     let prop = typeof source === "string" ? JSON.parse(source) : source;
     for (let val in prop) {
         if (val.match(/\d+/)) {
--- task3/scripts/main.js
+++ task3/scripts/main.js
@@ -1,9 +1,16 @@
 import data from '../data/data.json';
+import logo from '../images/logo.png';
 import 'babel-polyfill';
 import 'whatwg-fetch';
 import NewsApi from './newsApi';
 
 (() => {
+    const header = document.querySelector('header .wrapper');
+    const image = document.createElement("img");
+
+    image.src = logo;
+    header.prepend(image);
+
     const baseUrl = "https://newsapi.org/v2";
     const apiKey = "762fef256cf642a3bc9c21296a4ed824";
     console.log(JSON.stringify(data));
