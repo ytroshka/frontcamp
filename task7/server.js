5f3c86ece3f3c6a13873af782e5f5f3525c80e92
Yauheni Troshka <yauheni_troshka@epam.com>
2018-01-29 15:37:43 +0300

    node.js first part

---
+++ task7/server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('./logger');
+const uniqid = require('uniqid');
+const util = require('util');
+
+const app = express();
+const data = require('./data/data.json');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  logger.info(`date: ${new Date().toLocaleTimeString()}, url: ${req.url}, method: ${req.method}`);
+  next();
+});
+
+app.listen(3010, () => {
+  console.log('listening on 3010');
+});
+
+app.route('/posts')
+  .get((req, res) => {
+    res.send(data);
+  })
+  .post((req, res) => {
+    const {author, content} = req.body;
+
+    const post = {
+      'id': uniqid(),
+      'author': author || 'User',
+      'content': content || 'Content of the post'
+    };
+
+    data.push(post);
+    res.send('post');
+  });
+
+app.route('/posts/:id')
+  .get((req, res) => {
+    res.send(data.filter(post => {
+      return post.id === req.params.id
+    })[0]);
+  })
+  .put((req, res) => {
+    const {author, content} = req.body;
+    const id = req.params.id;
+    const post = {
+      'id': id,
+      'author': author || 'User',
+      'content': content || 'Content of the post'
+    };
+
+    if (data.map((post) => post.id).indexOf(id) === -1) {
+      data.push(post);
+      res.send('post ' + id);
+    } else {
+      res.send(id + ' exists');
+    }
+  })
+  .delete((req, res) => {
+    const id = req.params.id;
+
+    if (data.map((post) => post.id).indexOf(id) !== -1) {
+      data.splice(id, 1);
+      res.send('delete ' + id);
+    } else {
+      res.send('nothing to delete');
+    }
+  });
+
+app.all('*', function (req, res) {
+  res.render('./index.ejs');
+});
